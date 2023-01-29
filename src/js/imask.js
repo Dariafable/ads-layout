c23c31daa0918aec0628ef620cca4b15ec91060d
Daria Nebylitsa <daranebylitsa@gmail.com>
2023-01-29 16:42:09 +0300

    added imask function

---
+++ src/js/imask.js
@@ -0,0 +1,8 @@
+export default function () {
+  let element = document.getElementById('phone');
+  let maskOptions = {
+    mask: '+7(000)000-00-00',
+    lazy: true,
+  };
+  let mask = new IMask(element, maskOptions);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,9 @@ import './slick.min';
 import burger from './burger.js';
 import dropdown from './dropdown.js';
 import slider from './slider.js';
+import imask from './imask.js';
 
 burger();
 dropdown();
 slider();
-carouselDescription();
+imask();
